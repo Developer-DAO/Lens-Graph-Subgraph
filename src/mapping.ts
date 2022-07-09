9b327a86e79e426ac1ce998bd245182f374b28b5
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2022-07-09 17:12:22 +0530

    Add eventHandlers

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,7 +5,8 @@ import {
   BeaconUpgraded,
   Upgraded
 } from "../generated/lensproxy/lensproxy"
-import {CollectNFTDeployed, FollowNFTTransferred} from "../generated/LensHub/LensHub"
+import {CollectNFTDeployed, FollowNFTTransferred, ProfileCreated} from "../generated/LensHub/LensHub"
+import { Profile } from '../generated/schema'
 
 export function handleAdminChanged(event: AdminChanged): void {
 }
@@ -22,4 +23,21 @@ export function handleCollectNFTDeployed(event: CollectNFTDeployed): void {
 
 export function handleFollowNFTTransferred(event: FollowNFTTransferred): void {
 
+}
+
+export function handleProfileCreated(event: ProfileCreated): void {
+  let profile = Profile.load(event.params.profileId.toString())
+  
+  if(!profile) {
+    profile = new Profile(event.params.profileId.toString())
+    // profile.name = event.params.creator
+    // profile.bio = event.params.
+    // profile.followNftAddress = event.params.
+    profile.metadata = event.params.followNFTURI
+    profile.handle = event.params.handle
+    profile.onwnedBy = event.params.creator.toHexString()
+    profile.isDefault = true
+    profile.isFollowedByMe = false
+    profile.save()
+  }
 }
